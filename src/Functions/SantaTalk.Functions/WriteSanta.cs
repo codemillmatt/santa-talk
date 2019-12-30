9407580e67140fd469e09e53eb78c7421c13352f
Logesh Palani <logesh.01@hotmail.com>
2019-12-30 23:20:48 +0530

    Updated

--- src/Functions/SantaTalk.Functions/WriteSanta.cs
+++ src/Functions/SantaTalk.Functions/WriteSanta.cs
@@ -1,3 +1,18 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
+using System.Collections.Specialized;
+using System.Linq;
+using Microsoft.Rest.TransientFaultHandling;
+using SantaTalk.Models;
+
 namespace SantaTalk.Functions
 {
     public static class WriteSanta
