479986ce9e1f69fb29686b8aa1a00e70f00c6b2b
David Guesewell <d.guesewell@web.de>
2020-01-05 13:34:15 +0100

    Add functionality to scan handwritten letters.

--- src/SantaTalk.Android/MainActivity.cs
+++ src/SantaTalk.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Plugin.CurrentActivity;
 
 namespace SantaTalk.Droid
 {
@@ -22,6 +23,7 @@ namespace SantaTalk.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             FFImageLoading.Forms.Platform.CachedImageRenderer.Init(true);
+            CrossCurrentActivity.Current.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
